3dad20af5d365a2345191f8825c0bbf5d22ac057
chidi <chidi.devsq@olotusquare.co>
2019-03-28 22:59:55 +0100

    first Commit

---
+++ src/datepicker.js
@@ -0,0 +1,289 @@
+const dateInput = document.querySelector('.datepicker');
+const warning = document.querySelector('.warning');
+const p = document.querySelector('#calendar > p');
+const checkBox = document.querySelector('input[type=checkbox]');
+const submitInput = document.querySelector('input[type=submit]');
+const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'];
+let now, selectedTime, curObj;
+
+/**
+ * Creating Appointment Object
+ */
+class Appointment {
+    constructor(date) {
+        let _date = date;
+        this.edit = (message, date = _date) => {
+            if (date === _date) this.set(message);
+            else {
+                this.delete();
+                _date = date;
+                this.set(message);
+            }
+        };
+        this.get = () => {
+            return localStorage.getItem(_date);
+        };
+        this.getDate = () => {
+            return _date;
+        };
+        this.delete = () => {
+            localStorage.removeItem(_date);
+        };
+        this.set = (message) => {
+            localStorage.setItem(_date, message);
+        }
+    }
+}
+
+/**
+ * Appointment actions
+ * @param date
+ * @returns {boolean}
+ */
+const appointmentExist = date => {
+    let check;
+    if ((new Appointment(date)).get()) {
+        if (curObj) check = date !== curObj.getDate();
+        else check = true;
+    } else check = false;
+    return check;
+};
+
+const appoinmentExistsAction = (display, check) => {
+    warning.style.display = display;
+    checkboxSelect(check);
+};
+
+const formatDate = (d, m, y) => {
+    let day = d.toString().length < 2 ? '0' + d : d;
+    let mth = m.toString().length < 2 ? '0' + m : m;
+    return `${day}-${mth}-${y}`;
+};
+
+const getMonthYearString = dt => {
+    return monthArr[dt.getMonth()] + ' ' + dt.getFullYear();
+};
+
+/**
+ * Choose date in datepicker
+ */
+const chooseDate = e => {
+    let targ;
+    if (e.target) targ = e.target;
+    else if (e.srcElement) targ = e.srcElement;
+    if (targ.nodeType == 3) targ = targ.parentNode;
+
+    let div = targ.parentNode.parentNode.parentNode.parentNode.parentNode;
+    let idOfTextbox = div.getAttribute('datepickertextbox');
+    let textbox = document.getElementById(idOfTextbox);
+    if (targ.value == '<' || targ.value == '>') {
+        createCalendar(div, new Date(targ.getAttribute('date')));
+        return;
+    }
+    selectedDate = new Date(targ.getAttribute('date'));
+    textbox.value = formatDate(selectedDate.getDate(), selectedDate.getMonth() + 1, selectedDate.getFullYear());
+    let today = new Date();
+    now = (new Date(today.getMonth() + 1 + '/' + today.getDate() + '/' + today.getFullYear())).getTime();
+    selectedTime = selectedDate.getTime();
+
+    /**
+     * Check if the time is in the past
+     */
+    if (now > selectedTime) {
+        dateInput.classList.add('input-error');
+        p.style.display = 'block';
+        warning.style.display = 'none';
+        submitInputToggle(false);
+        return;
+    } else {
+        submitInputToggle();
+        dateInput.classList.remove('input-error');
+        p.style.display = 'none';
+    }
+    /**
+     * check if appointment exists for the current date
+     */
+    if (appointmentExist(textbox.value)) appoinmentExistsAction('block', true);
+    else appoinmentExistsAction('none', false);
+
+    div.parentNode.removeChild(div); // Remove the dropdown box
+    datepickerD = null;
+};
+
+/**
+ * Parse a date
+ */
+const parseMyDate = d => {
+    if (d == "") return new Date('NotADate'); // For Safari
+    let a = d.split('-');
+    if (a.length != 3) return new Date(d);
+    let m = -1;
+    if (a[1] == 'Jan') m = 0;
+    if (a[1] == 'Feb') m = 1;
+    if (a[1] == 'Mar') m = 2;
+    if (a[1] == 'Apr') m = 3;
+    if (a[1] == 'May') m = 4;
+    if (a[1] == 'Jun') m = 5;
+    if (a[1] == 'Jul') m = 6;
+    if (a[1] == 'Aug') m = 7;
+    if (a[1] == 'Sep') m = 8;
+    if (a[1] == 'Oct') m = 9;
+    if (a[1] == 'Nov') m = 10;
+    if (a[1] == 'Dec') m = 11;
+    if (m < 0) return new Date(d);
+    return new Date(a[2], m, a[0], 0, 0, 0, 0);
+};
+
+/**
+ * creating calendar for a given month
+ */
+const createCalendar = (div, month) => {
+    const idOfTextbox = div.getAttribute('datepickertextbox');
+    const textbox = document.getElementById(idOfTextbox);
+    const tbl = document.createElement('table');
+    const topRow = tbl.insertRow(-1);
+    let ttd = topRow.insertCell(-1);
+    const lastMonthBn = document.createElement('input');
+    lastMonthBn.type = 'button';
+    ttd.appendChild(lastMonthBn);
+    lastMonthBn.value = '<';
+    lastMonthBn.onclick = chooseDate;
+    lastMonthBn.setAttribute('date', new Date(month.getFullYear(), month.getMonth() - 1, 1, 0, 0, 0, 0).toString());
+    ttd = topRow.insertCell(-1);
+    ttd.colSpan = 5;
+    const mon = document.createElement('input');
+    mon.type = 'text';
+    ttd.appendChild(mon);
+    mon.value = getMonthYearString(month);
+    mon.size = 15;
+    mon.disabled = 'disabled';
+    ttd = topRow.insertCell(-1);
+    const nextMonthBn = document.createElement('input');
+    nextMonthBn.type = 'button';
+    ttd.appendChild(nextMonthBn);
+    nextMonthBn.value = '>';
+    nextMonthBn.onclick = chooseDate;
+    nextMonthBn.setAttribute('date', new Date(month.getFullYear(), month.getMonth() + 1, 1, 0, 0, 0, 0).toString());
+    const daysRow = tbl.insertRow(-1);
+    daysRow.insertCell(-1).innerHTML = "Mon";
+    daysRow.insertCell(-1).innerHTML = "Tue";
+    daysRow.insertCell(-1).innerHTML = "Wed";
+    daysRow.insertCell(-1).innerHTML = "Thu";
+    daysRow.insertCell(-1).innerHTML = "Fri";
+    daysRow.insertCell(-1).innerHTML = "Sat";
+    daysRow.insertCell(-1).innerHTML = "Sun";
+
+    /**
+     * Make the calender
+     */
+    let selected = parseMyDate(textbox.value);
+    let today = new Date();
+    date = new Date(month.getFullYear(), month.getMonth(), 1, 0, 0, 0, 0);
+    let extras = (date.getDay() + 6) % 7;
+    date.setDate(date.getDate() - extras);
+    while (1) {
+        let tr = tbl.insertRow(-1);
+        for (i = 0; i < 7; i++) {
+            let td = tr.insertCell(-1);
+            let inp = document.createElement('input');
+            inp.type = 'button';
+            td.appendChild(inp);
+            inp.value = date.getDate();
+            inp.onclick = chooseDate;
+            inp.setAttribute('date', date);
+            if (date.getMonth() != month.getMonth()) {
+                if (inp.className) inp.className += ' ';
+                inp.className += 'othermonth';
+            }
+            if (date.getDate() == today.getDate() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()) {
+                if (inp.className) inp.className += ' ';
+                inp.className += 'today';
+            }
+            if (!isNaN(selected) && date.getDate() == selected.getDate() && date.getMonth() == selected.getMonth() && date.getFullYear() == selected.getFullYear()) {
+                if (inp.className) inp.className += ' ';
+                inp.className += 'selected';
+            }
+            date.setDate(date.getDate() + 1);
+        }
+
+        if (date.getMonth() != month.getMonth()) {
+            break;
+        }
+    }
+
+    /**
+     * Inserting newly made table
+     */
+    if (div.hasChildNodes()) {
+        div.replaceChild(tbl, div.childNodes[0]);
+    } else {
+        div.appendChild(tbl);
+    }
+};
+
+/**
+ * Show date function
+ */
+const showDatePicker = idOfTextbox => {
+    const textbox = document.getElementById(idOfTextbox);
+    x = textbox.parentNode.getElementsByTagName('div');
+    for (i = 0; i < x.length; i++) {
+        if (x[i].getAttribute('class') == 'datepickerdropdown') {
+            textbox.parentNode.removeChild(x[i]);
+            return false;
+        }
+    }
+
+    /**
+     * Grab the date, or use the current date if not valid
+     */
+    let date = parseMyDate(textbox.value);
+    if (isNaN(date)) date = new Date();
+
+    /**
+     * Create box
+     */
+    const div = document.createElement('div');
+    div.className = 'datepickerdropdown';
+    div.setAttribute('datepickertextbox', idOfTextbox);
+    createCalendar(div, date);
+    insertAfter(div, textbox);
+    return false;
+};
+
+/**
+ * Adds an item after an existing one
+ */
+const insertAfter = (newItem, existingItem) => {
+    if (existingItem.nextSibling) {
+        existingItem.parentNode.insertBefore(newItem, existingItem.nextSibling);
+    } else {
+        existingItem.parentNode.appendChild(newItem);
+    }
+};
+
+const datePickerInit = () => {
+    const allElements = document.getElementsByTagName("*");
+    for (i = 0; i < allElements.length; i++) {
+        let className = allElements[i].className;
+        if (className == 'datepicker' || className.indexOf('datepicker ') != -1 || className.indexOf(' datepicker') != -1) {
+            const a = document.createElement('a');
+            a.href = '#';
+            a.className = "datepickershow";
+            a.setAttribute('onclick', 'return showDatePicker("' + allElements[i].id + '")');
+            const img = document.createElement('img');
+            a.appendChild(img);
+            insertAfter(a, allElements[i]);
+        }
+    }
+};
+
+/**
+ * Hook into the page load event
+ */
+if (window.addEventListener) {
+    window.addEventListener('load', datePickerInit, false);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', datePickerInit);
+}
---
+++ src/script.js
@@ -0,0 +1,223 @@
+const tbody = document.querySelector('tbody');
+const alertWrapper = document.querySelector('.alert-wrapper');
+const appointmentBtns = document.querySelector('.appointment-btn');
+const mark = '&#10003;';
+const form = document.querySelector('#calendar');
+const appointmentWrapper = document.querySelector('.appointment-wrapper');
+let curElement, curDate, isEdit;
+const textInput = document.querySelector('.text');
+
+
+/**
+ *Month Function
+ */
+const getMonth = (n) => {
+    return monthArr[parseInt(n)];
+};
+
+const fullDate = date => {
+    let a = date.split('-');
+    return getDay(parseInt(a[0])) + ' ' + getMonth(a[1]) + ', ' + a[2];
+};
+
+/**
+ *Date Function
+ */
+const getDay = n => {
+    const toText = n.toString();
+    if (n === 0) return toText;
+    const lastDigit = n % 10;
+    let suffix = "th";
+    if (lastDigit === 1 || lastDigit === 2 || lastDigit === 3) {
+        const last2Char = toText.slice(-2);
+        if (last2Char !== "11" && last2Char !== "12" && last2Char !== "13") {
+            suffix = (lastDigit === 1) ? "st" : (lastDigit === 2) ? "nd" :
+                (lastDigit === 3) ? "rd" : '';
+        }
+    }
+    return toText + suffix;
+};
+
+/**
+ *Display Days of current Month
+ */
+let date = new Date();
+const month = date.getMonth();
+const today = date.getDate();
+let days = [];
+let newday = 1;
+let newTrNode, tdClass;
+document.querySelector('.current-date').innerHTML = `${getMonth(month)}, ${date.getFullYear()}`;
+while (date.getMonth() === month) {
+    if (newday % 5 === 1) {
+        var trNode = document.createElement("tr");
+        newTrNode = tbody.appendChild(trNode);
+    }
+
+    tdClass = today > newday ? "inactive" : "active";
+    newTrNode.innerHTML += `<td class="${tdClass}" data-date="${newday}">
+                                    <span>${newday}</span>
+                                    ${(new Appointment(formatDate(newday, month + 1, date.getFullYear()))).get() ? mark : ''}
+                                </td>`;
+    days.push(newday);
+    newday++;
+    date.setDate(newday);
+}
+/**
+ * Take care of remaining empty boxes
+ */
+let rem = newday % 5;
+while (rem <= 5) {
+    newTrNode.innerHTML += `<td class="empty"></td>`;
+    rem++;
+}
+const td = document.querySelectorAll('td');
+/**
+ * function definitions
+ */
+const closeModal = () => {
+    alertWrapper.classList.add('hide');
+    setTimeout(() => {
+        hideEl(alertWrapper);
+        hideEl(form);
+        hideEl(appointmentWrapper);
+        textInput.value = dateInput.value = '';
+        submitInput.disabled = false;
+    }, 500);
+    curObj = undefined;
+    warning.style.display = 'none';
+    dateInput.classList.remove('input-error');
+    p.style.display = 'none';
+};
+
+const showEl = el => {
+    el.classList.add('show')
+};
+
+const hideEl = el => {
+    el.classList.remove('hide');
+    el.classList.remove('show');
+};
+
+const edit = check => {
+    dateInput.disabled = check;
+    isEdit = check;
+};
+
+const checkboxSelect = (check = !checkBox.checked) => {
+    submitInput.disabled = check;
+    submitInputToggle(!check);
+};
+
+const submitInputToggle = (show = true) => {
+    if (show) submitInput.classList.remove('inactive');
+    else submitInput.classList.add('inactive');
+};
+
+
+/**
+ * Adding eventlistener to calender dates
+ */
+td.forEach(function (value) {
+    value.addEventListener("click", function () {
+        curElement = this;
+        curDate = curElement.dataset.date;
+        if (curElement.className === 'empty') return;
+        curObj = new Appointment(formatDate(curDate, month + 1, date.getFullYear()));
+        if (curElement.className === 'inactive' && !curObj.get()) return;
+        if (curObj.get()) {
+            //Disable edit and delete for past appointments
+            if (curElement.className === 'inactive') appointmentBtns.style.display = 'none';
+            else appointmentBtns.style.display = "block";
+            appointmentWrapper.querySelector('p').innerHTML = curObj.get();
+            appointmentWrapper.querySelector('h3').innerHTML = fullDate(curObj.getDate());
+            showEl(appointmentWrapper);
+        } else {
+            dateInput.value = curObj.getDate();
+            showEl(form);
+        }
+        edit(true);
+        showEl(alertWrapper)
+    });
+});
+/**
+ * Creating new Appointment
+ */
+document.querySelector('.create').addEventListener('click', () => {
+    edit(false);
+    showEl(form);
+    showEl(alertWrapper);
+});
+
+/**
+ *Saving calendar entry
+ */
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    if (now > selectedTime) return;
+    if (appointmentExist(dateInput.value) && !checkBox.checked) return;
+    submitInput.disabled = true;
+    if (curObj) curObj.edit(textInput.value);
+    else (new Appointment(dateInput.value)).edit(textInput.value);
+    /**
+     * Set Current Element and Current Date if form is in creation mode
+     */
+    let check = true;
+    if (!isEdit) {
+        let dateArr = dateInput.value.split('-');
+        let chosenMonth = parseInt(dateArr[1]);
+        let chosenDate = parseInt(dateArr[0]);
+        if (chosenMonth === month + 1) {
+            curElement = document.querySelector(`td[data-date="${chosenDate}"]`);
+            curDate = curElement.dataset.date;
+        }else check = false;
+    }
+    if(check) curElement.innerHTML = `<span>${curDate}</span> ${mark}`;
+    textInput.value = '';
+    closeModal();
+});
+
+/**
+ * Editing Calendar Entries
+ */
+document.querySelector('.appointment-btn > button:first-child').onclick = () => {
+    textInput.value = curObj.get();
+    dateInput.value = curObj.getDate();
+    hideEl(appointmentWrapper);
+    showEl(form);
+};
+
+/**
+ * Deleting Calendar Entries
+ */
+document.querySelector('.appointment-btn > button:last-child').onclick = () => {
+    curObj.delete();
+    const curElementSpan = curElement.querySelector('span');
+    curElement.innerHTML = curElementSpan.outerHTML;
+    closeModal();
+};
+
+
+/**
+ * Datepicker functionality
+ */
+let datepickerD;
+
+dateInput.addEventListener('focus', () => {
+    datepickerD = document.querySelector('.datepickerdropdown');
+    if (!datepickerD) {
+        showDatePicker("start_dt");
+        datepickerD = document.querySelector('.datepickerdropdown');
+    }
+});
+
+window.onload = () => {
+    document.querySelector('.datepickershow').style.display = 'none';
+};
+
+alertWrapper.addEventListener('click', e => {
+    if (datepickerD && !datepickerD.contains(e.target) && e.target !== dateInput && e.target.value !== '<' && e.target.value !== '>') {
+        datepickerD.outerHTML = '';
+        datepickerD = null;
+    }
+});
